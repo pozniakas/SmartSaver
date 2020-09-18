02c90a2343f8dbf2c4a32d88a25743f29f67b7a8
Aleksandras Maliuginas <aleksandrasm348@gmail.com>
2020-09-18 17:38:32 +0300

    Data model added

---
+++ models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace SmartSaver.models
+{
+    class Transaction
+    {
+        private DateTime Date { get; set; }
+        private decimal Sum { get; set; }
+        private string Purpose { get; set; }
+
+        public Transaction(DateTime date, decimal sum, string purpose)
+        {
+            Date = date;
+            Sum = sum;
+            Purpose = purpose;
+        }
+    }
+}
