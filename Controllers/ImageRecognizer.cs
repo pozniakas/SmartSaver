46ab82539703ea7caaa30d59ed4bec21a42f07bc
Aleksandras Maliuginas <aleksandrasm348@gmail.com>
2020-10-13 08:38:33 +0300

    Image recognized updated

--- Controllers/ImageRecognizer.cs
+++ Controllers/ImageRecognizer.cs
@@ -1,19 +1,36 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
 using Tesseract;
 
 namespace SmartSaver.Controllers
 {
     public class ImageRecognizer
     {
+        //Install-Package Tesseract -Version 4.1.0-beta1
+        private readonly string TessDataPath = @".\assets\tessdata\best";
+
         public void Recognize()
         {
-            string path = @".\assets\receipt1.jpg";
+            DirectoryInfo dir = new DirectoryInfo(@"..\..\..\assets\Receipts");
 
-            var ocrengine = new TesseractEngine(@".\assets\tessdata", "lit", EngineMode.Default);
-            var img = Pix.LoadFromFile(path);
-            //img = img.ConvertRGBToGray();
-            var res = ocrengine.Process(img);
-            Debug.Write(res.GetText());
+            foreach (var imgPath in dir.GetFiles("r1.jpg"))
+            {
+                Debug.WriteLine(imgPath);
+
+                using var ocrEngineBest = new TesseractEngine(TessDataPath, "lit", EngineMode.Default);
+                //ocrEngineBest.SetVariable("tessedit_char_whitelist", "0123456789,.");
+
+                using var img = Pix.LoadFromFile(imgPath.ToString());
+                var imgGray = img.ConvertRGBToGray();
+
+                var res = ocrEngineBest.Process(imgGray).GetText();
+
+                var destPath = imgPath.Directory.Parent.FullName + @"\receiptsTxt\" + imgPath.Name + ".txt";
+                Debug.WriteLine(destPath);
+                
+                File.WriteAllText(destPath, res);
+            }
         }
     }
 }
