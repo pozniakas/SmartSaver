f4b158656556212278b65196f5f082a84e97023a
imila <imila@DESKTOP-P03MSJS>
2020-10-20 12:25:24 +0300

    Add and update category to the database

--- Desktop/MyBudgetWindow.Designer.cs
+++ Desktop/MyBudgetWindow.Designer.cs
@@ -34,6 +34,8 @@
             this.txtActivity = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.txtAvailable = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.budgetAndCategoriesView = new System.Windows.Forms.DataGridView();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.budgetAndCategoriesView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -69,12 +71,6 @@
             this.txtAvailable.Name = "txtAvailable";
             this.txtAvailable.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             // 
-            // MyBudgetWindow
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(586, 262);
-            // 
             // budgetAndCategoriesView
             // 
             this.budgetAndCategoriesView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -86,10 +82,39 @@
             this.txtAvailable});
             this.budgetAndCategoriesView.Location = new System.Drawing.Point(0, 0);
             this.budgetAndCategoriesView.Name = "budgetAndCategoriesView";
-            this.budgetAndCategoriesView.Size = new System.Drawing.Size(585, 262);
+            this.budgetAndCategoriesView.Size = new System.Drawing.Size(586, 224);
             this.budgetAndCategoriesView.TabIndex = 0;
             this.budgetAndCategoriesView.Text = "dataGridView1";
+            this.budgetAndCategoriesView.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.budgetAndCategoriesView_CellEnter);
             this.budgetAndCategoriesView.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.budgetAndCategoriesView_CellValueChanged);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(502, 230);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(72, 27);
+            this.buttonAdd.TabIndex = 1;
+            this.buttonAdd.Text = "Add New";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(410, 230);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(74, 27);
+            this.buttonDelete.TabIndex = 2;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // MyBudgetWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 262);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.budgetAndCategoriesView);
             this.Name = "MyBudgetWindow";
             this.Text = "MyBudgetWindow";
@@ -106,5 +131,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn txtBudgeted;
         private System.Windows.Forms.DataGridViewTextBoxColumn txtActivity;
         private System.Windows.Forms.DataGridViewTextBoxColumn txtAvailable;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonDelete;
     }
 }
--- Desktop/MyBudgetWindow.cs
+++ Desktop/MyBudgetWindow.cs
@@ -42,28 +42,63 @@ namespace SmartSaver.Desktop
             foreach (var category in CategoryList)
             {
                 this.budgetAndCategoriesView.Rows.Add(category.Id, category.Title, category.Description, " ", " ");
-             
+
             }
         }
 
         private void budgetAndCategoriesView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
         {
-            DataGridViewRow dgwRow = new DataGridViewRow();
+            /*DataGridViewRow dgwRow = budgetAndCategoriesView.CurrentRow;
      
             if (budgetAndCategoriesView.CurrentRow != null)
             {
-               // DataGridViewRow dgwRow = new DataGridViewRow();
-                /* Category newCategory = new Category
-                 {
-                     Id = 1,
-                     Title = dgwRow.Cells["txtCategory"].Value.ToString(),
-                     Description = dgwRow.Cells["txtBudgeted"].Value.ToString()
 
-                 };*/
+                Debug.WriteLine(dgwRow.Cells["txtCategory"].Value == null ? " " : dgwRow.Cells["txtCategory"].Value.ToString());
+                Debug.WriteLine(Convert.ToInt32(dgwRow.Cells["txtActivity"].Value == null ? "0" : dgwRow.Cells["txtCategory"].Value.ToString()));
+                //  Debug.WriteLine(Convert.ToInt32(dgwRow.Cells["txtActivity"].Value == null ? "0" : dgwRow.Cells["txtCategory"].Value.ToString()));
+                // Debug.WriteLine(Convert.ToInt32(dgwRow.Cells["txtAvailable"].Value == null ? "0" : dgwRow.Cells["txtCategory"].Value.ToString()));
+                Category newCategory = new Category
+                {
+                    Id = 1,
+                    Title = dgwRow.Cells["txtCategory"].Value == null ? " ": dgwRow.Cells["txtCategory"].Value.ToString(),
+                    //Title = dgwRow.Cells["txtCategory"].Value.ToString(),
+                   // Description = dgwRow.Cells["txtBudgeted"].Value.ToString()
 
-                // db.AddCategory(newCategory);
+                };
 
-            }
+                db.AddCategory(newCategory);
+            }*/
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            
+             string title = budgetAndCategoriesView.Rows[budgetAndCategoriesView.RowCount - 2].Cells[1].Value.ToString();
+             /*int budgeted = Convert.ToInt32(budgetAndCategoriesView.Rows[budgetAndCategoriesView.RowCount - 2].Cells[2].Value.ToString());
+             int activity = Convert.ToInt32(budgetAndCategoriesView.Rows[budgetAndCategoriesView.RowCount - 2].Cells[3].Value.ToString());
+             int available = Convert.ToInt32(budgetAndCategoriesView.Rows[budgetAndCategoriesView.RowCount - 2].Cells[4].Value.ToString());*/
+            // Debug.WriteLine(title + budgeted + activity + available);
+
+             Database db = new Database();
+
+            Category newCategory = new Category
+            {
+                Title = title
+            };
+
+            db.AddCategory(newCategory);
+            MessageBox.Show("New category added successfully");
+
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void budgetAndCategoriesView_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
