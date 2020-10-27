ca5ea37669c6c94b1513885e8945a1c2611ac5d7
imila <imila@DESKTOP-P03MSJS>
2020-10-27 13:56:52 +0200

    AddGoalWindow review

--- Desktop/AddGoalWindow.cs
+++ Desktop/AddGoalWindow.cs
@@ -26,15 +26,15 @@ namespace SmartSaver.Desktop
 
         private void PrepareGoalListView()
         {
-            listView1.View = View.Details;
-            listView1.GridLines = true;
+            goalWindowListView.View = View.Details;
+            goalWindowListView.GridLines = true;
 
-            listView1.Columns.Add("Title", 100);
-            listView1.Columns.Add("Datetime", 70);
-            listView1.Columns.Add("Amount", 100);
-            listView1.Columns.Add("Details", 146);
-            listView1.Columns.Add("To save in a week", 120);
-            listView1.Columns.Add("Possibility", 100);
+            goalWindowListView.Columns.Add("Title", 100);
+            goalWindowListView.Columns.Add("Datetime", 70);
+            goalWindowListView.Columns.Add("Amount", 100);
+            goalWindowListView.Columns.Add("Details", 146);
+            goalWindowListView.Columns.Add("To save in a week", 120);
+            goalWindowListView.Columns.Add("Possibility", 100);
         }
 
         public void UpdateGoalList()
@@ -51,33 +51,38 @@ namespace SmartSaver.Desktop
 
         public string GoalPossibility(int profit, double worth)
         {
-            string Possibility;
             double profitAWeek = profit / 4;
             if (worth / profitAWeek <= 0.5)
             {
-                return Possibility = "Huge";
+                return "Huge";
             }
 
             if (worth / profitAWeek <= 0.8)
             {
-                return Possibility = "Real";
+                return "Real";
             }
 
             if (worth / profitAWeek <= 1)
             {
-                return Possibility = "Small";
+                return "Small";
             }
 
-            return Possibility = "Not real";
+            return "Not real";
+ 
         }
 
         private void PopulateGoalListView(IEnumerable<Goal> GoalList)
         {
-            listView1.Items.Clear();
+            goalWindowListView.Items.Clear();
 
             foreach (var goal in GoalList)
             {
-                var money = Decimal.ToInt32(goal.Amount) /
+                if (goal.Deadlinedate == null)
+                {
+                    continue;
+                }
+
+                var money = decimal.ToInt32(goal.Amount) /
                             (((DateTime)goal.Deadlinedate).Subtract(DateTime.UtcNow) / 7).Days;
                 var profit = 200;
                 var possibility = GoalPossibility(profit, money);
@@ -87,11 +92,11 @@ namespace SmartSaver.Desktop
                     goal.Description, money.ToString(), possibility
                 });
 
-                listView1.Items.Add(item);
+                goalWindowListView.Items.Add(item);
             }
         }
 
-        private void goalAmount_KeyPress(object sender, KeyPressEventArgs e)
+        private void goalMoney_KeyPress(object sender, KeyPressEventArgs e)
         {
             var ch = e.KeyChar;
 
