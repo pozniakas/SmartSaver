d661e2942b3d457c0556770b395f3a6e38c54e87
Aleksandras Maliuginas <aleksandrasm348@gmail.com>
2020-10-28 08:34:44 +0200

    Database updated

--- Models/postgresContext.cs
+++ Models/postgresContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 
@@ -26,7 +25,8 @@ namespace SmartSaver.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql(ConfigurationManager.ConnectionStrings["postgresDatabase"].ConnectionString);
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=postgres");
             }
         }
 
@@ -123,6 +123,7 @@ namespace SmartSaver.Models
                 entity.HasOne(d => d.Category)
                     .WithMany(p => p.Transaction)
                     .HasForeignKey(d => d.CategoryId)
+                    .OnDelete(DeleteBehavior.SetNull)
                     .HasConstraintName("fk_smartsaver.transaction__category_id");
             });
 
