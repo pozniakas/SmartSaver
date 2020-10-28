d45a17250179f4b7150a9d24511bc77dffef2381
imila <imila@DESKTOP-P03MSJS>
2020-10-28 20:19:37 +0200

    Foreach to linq

--- Desktop/MyBudgetWindow.cs
+++ Desktop/MyBudgetWindow.cs
@@ -55,19 +55,10 @@ namespace SmartSaver.Desktop
             int index = 0;
 
             foreach (var category in categoryList)
-            {   
-                Debug.WriteLine("CATEGORY: " + category.Id + " " + category.Title);
-                Debug.WriteLine("COUNT: " + category.Transaction.Count);
-                foreach (var transaction in transactionList)
-                {
-                    if (category.Id == transaction.CategoryId)
-                    {
-                        calc += transaction.Amount;
-                    }
-                    Debug.WriteLine("TRANSACTION: " + transaction.CategoryId +" " + transaction.Amount);
-                }
-               
-                Debug.WriteLine(category.Title + " " + calc);
+            {
+                calc += transactionList.Where(transaction => category.Id == transaction.CategoryId)
+                    .Sum(transaction => transaction.Amount);
+
                 budgetAndCategoriesView.Rows[index].Cells[3].Value = Math.Abs(calc);
                 budgetAndCategoriesView.Rows[index].Cells[4].Value = category.DedicatedAmount - Math.Abs(calc);
                 if (category.DedicatedAmount - calc < 0)
