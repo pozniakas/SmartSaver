b8af2f00ad26184767dc029831dbf4bb4ed02e82
Aleksandras Maliuginas <aleksandrasm348@gmail.com>
2020-10-30 12:22:50 +0200

    Web application added.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,14 @@ namespace SmartSaver
         /// <summary>
         ///     The main entry point for the application.
         /// </summary>
-        [STAThread]
-        private static void Main()
-        {
-            new ImageRecognizer().IsLoaded();
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
-        }
+        //[STAThread]
+        //private static void Main()
+        //{
+        //    new ImageRecognizer().IsLoaded();
+        //    Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        //    Application.EnableVisualStyles();
+        //    Application.SetCompatibleTextRenderingDefault(false);
+        //    Application.Run(new Main());
+        //}
     }
 }
