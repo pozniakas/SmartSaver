37a0100fe9fcfafa94be2e469e3d283ae3b531df
Aleksandras Maliuginas <aleksandrasm348@gmail.com>
2020-10-31 18:42:01 +0200

    Add main controllers

---
+++ WebAPI/Controllers/GoalsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoalsController : ControllerBase
+    {
+        private readonly postgresContext _context;
+
+        public GoalsController(postgresContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Goals
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Goal>>> GetGoal()
+        {
+            return await _context.Goal.ToListAsync();
+        }
+
+        // GET: api/Goals/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Goal>> GetGoal(long id)
+        {
+            var goal = await _context.Goal.FindAsync(id);
+
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            return goal;
+        }
+
+        // PUT: api/Goals/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGoal(long id, Goal goal)
+        {
+            if (id != goal.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(goal).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GoalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Goals
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Goal>> PostGoal(Goal goal)
+        {
+            _context.Goal.Add(goal);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGoal", new { id = goal.Id }, goal);
+        }
+
+        // DELETE: api/Goals/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Goal>> DeleteGoal(long id)
+        {
+            var goal = await _context.Goal.FindAsync(id);
+            if (goal == null)
+            {
+                return NotFound();
+            }
+
+            _context.Goal.Remove(goal);
+            await _context.SaveChangesAsync();
+
+            return goal;
+        }
+
+        private bool GoalExists(long id)
+        {
+            return _context.Goal.Any(e => e.Id == id);
+        }
+    }
+}
