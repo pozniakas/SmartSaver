41d8986aecf96e6840b8d80fd51e2fa5852a24d9
imila <imila@DESKTOP-P03MSJS>
2020-11-04 20:12:49 +0200

    Pie chart added.

--- WebApplication/ClientApp/src/components/Home.js
+++ WebApplication/ClientApp/src/components/Home.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import { LineChart, Line } from 'recharts';
 
+import {
+  PieChart, Pie, Legend, Tooltip,
+} from 'recharts';
+
+const data01 = [
+  { name: 'Groceries', value: 400 },
+  { name: 'Wage & Salary', value: 300 },
+  { name: 'Transport', value: 278 },
+  { name: 'Entertainment', value: 50 },
+];
+
 const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400},{name: 'Page A', uv: 400, pv: 2400, amt: 2400},{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
 
 export class Home extends Component {
@@ -8,11 +19,18 @@ export class Home extends Component {
 
   render () {
     return (
-      <div>
+
+      <div className = "App">
         <h1>Smart Saver</h1>
-        <LineChart width={400} height={400} data={data}>
+        <LineChart width={500} height={500} data={data}>
         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
         </LineChart>
+        <h2>Categories chart (eur.)</h2>
+        <PieChart width={400} height={400}>
+        <Pie dataKey="value" isAnimationActive={false} data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label />
+        <Tooltip />
+      </PieChart>
+      <script src="./app.js"></script>
       </div>
     );
   }
