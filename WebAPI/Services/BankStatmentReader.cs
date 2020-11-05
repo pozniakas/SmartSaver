a1753ee61aa805f335c05f909ec645816c6689ee
Arijus Rancas <arijus.rancas@mif.stud.vu.lt>
2020-11-05 11:22:36 +0200

    All Lithuanian bank statment reader

--- WebAPI/Services/BankStatmentReader.cs
+++ WebAPI/Services/BankStatmentReader.cs
@@ -18,7 +18,6 @@ namespace WebAPI.Services
         private readonly StreamReader stream;
         private readonly CsvReader csv;
         private IEnumerable<string> header;
-
         public BankStatmentReader(StreamReader streamReader)
         {
             stream = streamReader;
@@ -65,10 +64,11 @@ namespace WebAPI.Services
                     break;
                 // Luminor EN
                 case "Date,Time,Amount":
-                    //_readRecord = LuminorEnReadRecord;
+                    _readRecord = new ReadRecord(LuminorEnReadRecord);
                     break;
                 // Luminor LT
                 case "Data,Laikas,Suma":
+                    _readRecord = new ReadRecord(LuminorLtReadRecord);
                     break;
                 // Seb EN
                 case "INSTRUCTION ID,DATE,CURRENCY":
@@ -76,6 +76,7 @@ namespace WebAPI.Services
                     break;
                 // Seb LT
                 case "DOK NR.,DATA,VALIUTA":
+                    _readRecord = new ReadRecord(SebLtReadRecord);
                     break;
             }
         }
@@ -115,6 +116,40 @@ namespace WebAPI.Services
         {
             return debitOrCredit.ToLower() == "d" ? -sum : sum;
         }
+        private Transaction? LuminorLtReadRecord()
+        {
+            /// Operacijos/Balanso Tipas,Data,Laikas,Suma,Ekvivalentas,C/D,Orig. suma,Orig. valiuta,
+            /// Operacijos dok. Nr.,Operacijos eilutė (identifikatorius),Kliento kodas gavėjo informac. sistemoje,
+            /// Įmokos kodas,Mokėjimo paskirtis,Kitos pusės BIC,Kitos pusės Kredito įstaigos pavadinimas,Kitos pusės Sąskaitos Nr.,
+            /// Kitos pusės Pavadinimas,Kitos pusės Asmens kodas/Registracijos Nr.,Kitos pusės Kliento kodas mokėtojo informacinėje sistemoje
+
+            return csv.TryGetField("Orig. suma", out decimal sum) && csv.TryGetField("C/D", out string debitOrCredit)
+                ? new Transaction
+                {
+                    TrTime = csv.GetField<DateTime>("Data"),
+                    Amount = GetAmout(sum, debitOrCredit),
+                    CounterParty = csv.GetField("Kitos pusės Pavadinimas"),
+                    Details = csv.GetField("Mokėjimo paskirtis")
+                }
+                : null;
+        }
+
+        private Transaction LuminorEnReadRecord()
+        {
+            ///Transaction type,Date,Time,Amount,Equivalent,C/D,Orig. amount,Orig. currency,Document number,Transaction ID,
+            ///Customer‘s code in beneficiary IS,Payment code,Payment details,Counterparty BIC,Counterparty Designation of counterparties credit institution,
+            ///Counterparty Account number,	Counterparty Designation,Counterparty Reg No.,Counterparty Customer‘s code in payer IS,Ultimate Payer Account number
+
+            return csv.TryGetField("Orig. amount", out decimal sum) && csv.TryGetField("C/D", out string debitOrCredit)
+                ? new Transaction
+                {
+                    TrTime = csv.GetField<DateTime>("Date"),
+                    Amount = GetAmout(sum, debitOrCredit),
+                    CounterParty = csv.GetField("Counterparty Designation"),
+                    Details = csv.GetField("Payment details")
+                }
+                : null;
+        }
 
         private Transaction? SwedbankLtReadRecord()
         {
@@ -133,6 +168,7 @@ namespace WebAPI.Services
 
         private Transaction SebEnReadRecord()
         {
+
             /// INSTRUCTION ID,DATE,CURRENCY,AMOUNT,COUNTERPARTY,DEBTOR/CREDITOR ID,ACCOUNT NO,
             /// CREDIT INSTITUTION NAME,CREDIT INSTITUTION SWIFT,DETAILS OF PAYMENTS,TRANSACTION CODE,
             /// DOCUMENT DATE,TRANSACTION TYPE,REFERENCE NO,DEBIT/CREDIT,AMOUNT IN ACCOUNT CURRENCY,ACCOUNT NO,ACCOUNT CURRENCY
@@ -146,5 +182,22 @@ namespace WebAPI.Services
                 }
                 : null;
         }
+        private Transaction SebLtReadRecord()
+        {
+            ///DOK NR.,DATA,VALIUTA,SUMA,MOKĖTOJO ARBA GAVĖJO PAVADINIMAS,MOKĖTOJO ARBA GAVĖJO IDENTIFIKACINIS KODAS,SĄSKAITA,
+            ///KREDITO ĮSTAIGOS PAVADINIMAS,KREDITO ĮSTAIGOS SWIFT KODAS,MOKĖJIMO PASKIRTIS,TRANSAKCIJOS KODAS,
+            ///DOKUMENTO DATA,TRANSAKCIJOS TIPAS,NUORODA,DEBETAS/KREDITAS,SUMA SĄSKAITOS VALIUTA,SĄSKAITOS NR,SĄSKAITOS VALIUTA
+
+            return csv.TryGetField("SUMA", out decimal sum) && csv.TryGetField("DEBETAS/KREDITAS", out string debitOrCredit)
+                ? new Transaction
+                {
+                    TrTime = csv.GetField<DateTime>("DATA"),
+                    Amount = GetAmout(sum, debitOrCredit),
+                    CounterParty = csv.GetField("MOKĖTOJO ARBA GAVĖJO PAVADINIMAS"),
+                    Details = csv.GetField("MOKĖJIMO PASKIRTIS")
+                }
+                : null;
+        }
+
     }
 }
