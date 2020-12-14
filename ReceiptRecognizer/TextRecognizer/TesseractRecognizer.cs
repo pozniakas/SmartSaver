76a0fdcae2bfbb1f839fe863d70a0f4ddd04cfc5
AleksandrasMaliuginas <aleksandrasm348@gmail.com>
2020-12-14 13:09:06 +0200

    Image Recognition fixed

--- ReceiptRecognizer/TextRecognizer/TesseractRecognizer.cs
+++ ReceiptRecognizer/TextRecognizer/TesseractRecognizer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Tesseract;
@@ -9,25 +10,30 @@ namespace Recognizer.TextRecognizer
 {
     public class TesseractRecognizer : ITextRecognizer
     {
-        private const string TessDataPath = @".\assets\tessdata\best";
+        private const string TessDataPath = @"..\ReceiptRecognizer\assets\tessdata\best";
+        private bool isFirstTry = true;
 
         public async Task<string> GetText(Bitmap image, string language)
         {
             try
             {
+                var dir = new DirectoryInfo(TessDataPath);
                 using var ocrEngineBest = new TesseractEngine(TessDataPath, language, EngineMode.Default);
                 using var img = Pix.LoadFromMemory(ImageToByte(image));
                 var imgGray = img.ConvertRGBToGray();
 
-                var extractText = new Task<string>(() =>
-                    ocrEngineBest.Process(imgGray).GetText());
+                var extractText = new Task<string>(() => ocrEngineBest.Process(imgGray).GetText());
                 extractText.Start();
 
                 return await extractText;
             } 
-            catch (TesseractException)
+            catch (TesseractException ex)
             {
-                return await GetText(image, "lit");
+                if (isFirstTry)
+                {
+                    return await GetText(image, "lit");
+                }
+                throw ex;
             }
         }
 
