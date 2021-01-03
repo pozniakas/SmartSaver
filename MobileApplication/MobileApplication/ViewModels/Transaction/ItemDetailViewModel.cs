19c9e5fb88d1c60c5e5c1225f7c34aa41fa84fb3
Simona <kassperaviciute@gmail.com>
2021-01-03 21:06:37 +0200

    Transaction details category changeable

--- MobileApplication/MobileApplication/ViewModels/Transaction/ItemDetailViewModel.cs
+++ MobileApplication/MobileApplication/ViewModels/Transaction/ItemDetailViewModel.cs
@@ -5,33 +5,56 @@ using MobileApplication.Models;
 using Xamarin.Forms;
 using DbEntities.Entities;
 using MobileApplication.Services.Rest;
+using System.Collections.ObjectModel;
 
 namespace MobileApplication.ViewModels
 {
     public class ItemDetailViewModel : BaseViewModel
     {
         private readonly IRestService<Transaction> RestService;
+        private readonly IRestService<Category> CategoryRestService;
+        public ObservableCollection<Category> Categories { get; set; }
+
+        private Category category;
         public ItemDetailViewModel(Transaction transaction) {
             Id = transaction.Id.ToString();
             Amount = transaction.Amount.ToString();
             Details = transaction.Details;
             TrTime = transaction.TrTime;
-            if (transaction.Category != null)
-            {
-                Category = transaction.Category.Title;
-            }
-            else
-            {
-                Category = "not set";
-            }
+                Categor = transaction.Category;
+            if (Categor == null) CategoryName = "not set";
+            else CategoryName = Categor.Title;
             CounterParty = transaction.CounterParty;
 
+
             RestService = new RestService<Transaction>("api/Transactions/");
+            CategoryRestService = new RestService<Category>("api/Categories");
+            Categories = new ObservableCollection<Category>();
+            _ = ExecuteLoadCategoryItemsCommand();
 
             SaveCommand = new Command(OnSave,ValidateSave);
             DeleteCommand = new Command(OnDelete);
             this.PropertyChanged +=
                 (_, __) => SaveCommand.ChangeCanExecute();
+        }
+        async Task ExecuteLoadCategoryItemsCommand()
+        {
+            IsBusy = true;
+            Categories.Clear();
+
+            try
+            {
+                var items = await CategoryRestService.RefreshDataAsync();
+                items.ForEach(category => Categories.Add(category));
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
         public Command SaveCommand { get; }
@@ -40,14 +63,19 @@ namespace MobileApplication.ViewModels
         private string amount;
         private string details;
         private DateTime trTime;
-        private string category;
         private string counterParty;
+        private string categoryName;
         public string Id { get; set; }
 
         public string Amount
         {
             get => amount;
             set => SetProperty(ref amount, value);
+        }
+        public string CategoryName
+        {
+            get => categoryName;
+            set => SetProperty(ref categoryName, value);
         }
 
         public string Details
@@ -62,10 +90,15 @@ namespace MobileApplication.ViewModels
             set => SetProperty(ref trTime, value);
         }
 
-        public string Category
+        public Category Categor
         {
-            get => category;
-            set => SetProperty(ref category, value);
+            get {
+                return category; }
+            set
+            {
+                category = value;
+                OnPropertyChanged("Categor");
+            }
         }
         public string CounterParty
         {
@@ -91,7 +124,8 @@ namespace MobileApplication.ViewModels
                 TrTime = TrTime,
                 Amount = amountInDecimal,
                 Details = Details,
-                CounterParty = CounterParty
+                CounterParty = CounterParty,
+                Category = Categor  
             };
 
             IsBusy = true;
